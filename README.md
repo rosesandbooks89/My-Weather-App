b28faf4996239518b88c4c2785e11b5174540ef8
rosesandbooks89 <rosesandbooks89@gmail.com>
2023-05-25 18:11:41 -0500

    updating readme

--- README.md
+++ README.md
@@ -2,23 +2,24 @@
 
 ## Your Task
 
-My task was to create a weather app that allows a user to look for weather from multiple cities. 
+My task was to create a weather app that allows a user to look for weather from multiple cities.
 
 I used https://openweathermap.org/forecast5 to get api key as well as the calls to get the weather data
 
 ## Acceptance Criteria
 
-* GIVEN a weather dashboard with form inputs
-* WHEN I search for a city
-* THEN I am presented with current and future conditions for that city and that city is added to the search history
-* WHEN I view current weather conditions for that city
-* THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, and the the wind speed
-* WHEN I view future weather conditions for that city
-* THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, the wind speed, and the humidity
-* WHEN I click on a city in the search history
-* THEN I am again presented with current and future conditions for that city
+- GIVEN a weather dashboard with form inputs
+- WHEN I search for a city
+- THEN I am presented with current and future conditions for that city and that city is added to the search history
+- WHEN I view current weather conditions for that city
+- THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, and the the wind speed
+- WHEN I view future weather conditions for that city
+- THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, the wind speed, and the humidity
+- WHEN I click on a city in the search history
+- THEN I am again presented with current and future conditions for that city
 
 ## Struggles
+
 This weeks assignment proved to be very difficult knowing where to get the code. After working with tutor, teammates BCS and TAs, getting code from assignments is acceptable.
 
 ## Link to Deployed Application
@@ -26,5 +27,6 @@ This weeks assignment proved to be very difficult knowing where to get the code.
 https://rosesandbooks89.github.io/My-weather-app/
 
 ## Screenshots
-![alt text](./assets/images/image1.png)
-![alt text](./assets/images/image2.png)
+
+![alt text](./assets/images/Screenshot%202023-05-25%20180315.png)
+
--- assets/script.js
+++ assets/script.js
@@ -4,119 +4,122 @@ var searchHistory = [];
 
 // Function to fetch current weather
 function getCurrentWeather(cityName) {
-    var currentWeatherURL = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}`;
+  var currentWeatherURL = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}`;
 
-    fetch(currentWeatherURL)
-        .then(response => response.json())
-        .then(data => {
-            displayCurrentWeather(data);
-            getFiveDayWeather(cityName);
-        });
-        document.getElementById("currentWeather").removeAttribute("style");
+  fetch(currentWeatherURL)
+    .then((response) => response.json())
+    .then((data) => {
+      displayCurrentWeather(data);
+      getFiveDayWeather(cityName);
+    });
+  document.getElementById("currentWeather").removeAttribute("style");
 }
 
 // Function to fetch the 5-day weather forecast
 function getFiveDayWeather(cityName) {
-    var fiveDayWeatherURL = `https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=${apiKey}`;
+  var fiveDayWeatherURL = `https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=${apiKey}`;
 
-    fetch(fiveDayWeatherURL)
-        .then(response => response.json())
-        .then(data => {
-            displayFiveDayWeather(data.list);
-        });
+  fetch(fiveDayWeatherURL)
+    .then((response) => response.json())
+    .then((data) => {
+      displayFiveDayWeather(data.list);
+    });
 }
 
 // TODO: Default funtion (not sure)
 
-// TODO: function for temperture converter may be needed. / got from W3Schools 
+// TODO: function for temperture converter may be needed. / got from W3Schools
 function temperatureConverter(temp) {
-    return Math.round(((temp - 273.15) * 1.8) + 32);
+  return Math.round((temp - 273.15) * 1.8 + 32);
 }
 
-
 // Function to display the current weather
-// TODO: convert temperture to Fahrenheit and add icon weather to function. 
+// TODO: convert temperture to Fahrenheit and add icon weather to function.
 function displayCurrentWeather(data) {
-    var cityName = document.getElementById("cityName");
-    var date = document.querySelector(".date-");
-    var temp = document.getElementById("dTemp-0");
-    var wind = document.getElementById("dWind-0");
-    var humidity = document.getElementById("dHumidity-0");
-        document.getElementById("img-");
+  var cityName = document.getElementById("cityName");
+  var date = document.querySelector(".date-");
+  var temp = document.getElementById("dTemp-0");
+  var wind = document.getElementById("dWind-0");
+  var humidity = document.getElementById("dHumidity-0");
+  document.getElementById("img-");
 
-    
-    cityName.textContent = data.name;
-    temp.textContent = "Temperature: " + temperatureConverter(data.main.temp) + "°F";
-    wind.textContent = "Wind: " + data.wind.speed;
-    humidity.textContent = "Humidity: " + data.main.humidity;
-    console.log("Current Weather Data:", data);
+  cityName.textContent = data.name;
+  temp.textContent =
+    "Temperature: " + temperatureConverter(data.main.temp) + "°F";
+  wind.textContent = "Wind: " + data.wind.speed;
+  humidity.textContent = "Humidity: " + data.main.humidity;
+  console.log("Current Weather Data:", data);
 }
 
 // Function to display the 5-day weather forecast
 // TODO: convert temperture to Fahrenheit
 function displayFiveDayWeather(weatherList) {
-    for (var i = 0; i < 5; i++) {
-        var date = document.querySelector(".date-" + (i + 1));
-        var temp = document.getElementById("dTemp-" + (i + 1));
-        var wind = document.getElementById("dWind-" + (i + 1));
-        var humidity = document.getElementById("dHumidity-" + (i + 1));
-        var icon = document.getElementById("img-" + (i + 1));
-        var weatherData = weatherList[i];
-        
-        date.textContent = new Date(weatherData.dt * 1000).toLocaleDateString();
-        temp.textContent = "Temperature: " + temperatureConverter(weatherData.main.temp) + "°F";
-        wind.textContent = "Wind: " + weatherData.wind.speed;
-        humidity.textContent = "Humidity: " + weatherData.main.humidity;
-        icon.src = "https://openweathermap.org/img/wn/" + weatherData.weather[0].icon + ".png";
-        icon.alt = weatherData.weather[0].description;
-        document.getElementById("forecast").removeAttribute("style");
-    }
+  for (var i = 0; i < 5; i++) {
+    var date = document.querySelector(".date-" + (i + 1));
+    var temp = document.getElementById("dTemp-" + (i + 1));
+    var wind = document.getElementById("dWind-" + (i + 1));
+    var humidity = document.getElementById("dHumidity-" + (i + 1));
+    var icon = document.getElementById("img-" + (i + 1));
+    var weatherData = weatherList[i];
+
+    date.textContent = new Date(weatherData.dt * 1000).toLocaleDateString();
+    temp.textContent =
+      "Temperature: " + temperatureConverter(weatherData.main.temp) + "°F";
+    wind.textContent = "Wind: " + weatherData.wind.speed;
+    humidity.textContent = "Humidity: " + weatherData.main.humidity;
+    icon.src =
+      "https://openweathermap.org/img/wn/" +
+      weatherData.weather[0].icon +
+      ".png";
+    icon.alt = weatherData.weather[0].description;
+    document.getElementById("forecast").removeAttribute("style");
+  }
 }
 
 // Event listener for the search button
 document.getElementById("search-form").addEventListener("submit", function (e) {
-    e.preventDefault();
-    var city = cityInput.value.trim();
+  e.preventDefault();
+  var city = cityInput.value.trim();
 
-    if (city) {
-        getCurrentWeather(city);
-        if (!searchHistory.includes(city)) {
-            searchHistory.push(city);
-            saveSearchHistory();
-            displaySearchHistory();
-        }
+  if (city) {
+    getCurrentWeather(city);
+    if (!searchHistory.includes(city)) {
+      searchHistory.push(city);
+      saveSearchHistory();
+      displaySearchHistory();
     }
-    cityInput.value = "";
+  }
+  cityInput.value = "";
 });
 
 // Function to save search history to local storage
 function saveSearchHistory() {
-    localStorage.setItem("history", JSON.stringify(searchHistory));
+  localStorage.setItem("history", JSON.stringify(searchHistory));
 }
 
 // Function to display search history
 function displaySearchHistory() {
-    var historyList = document.querySelector(".history");
-    historyList.innerHTML = "";
+  var historyList = document.querySelector(".history");
+  historyList.innerHTML = "";
 
-    searchHistory.forEach(function (city) {
-        var li = document.createElement("li");
-        li.textContent = city;
-        li.addEventListener("click", function () {
-            cityInput.value = city;
-            getCurrentWeather(city);
-        });
-        historyList.appendChild(li);
+  searchHistory.forEach(function (city) {
+    var li = document.createElement("li");
+    li.textContent = city;
+    li.addEventListener("click", function () {
+      cityInput.value = city;
+      getCurrentWeather(city);
     });
+    historyList.appendChild(li);
+  });
 }
 
 // Load search history from local storage
 function loadSearchHistory() {
-    var history = localStorage.getItem("history");
-    if (history) {
-        searchHistory = JSON.parse(history);
-        displaySearchHistory();
-    }
+  var history = localStorage.getItem("history");
+  if (history) {
+    searchHistory = JSON.parse(history);
+    displaySearchHistory();
+  }
 }
 
 // Call the loadSearchHistory function when the page loads
